89aaf42803ca58b0ed1bdb9545c5e50307b63cc7
sylwow <Sylwo1003@gmail.com>
2022-05-06 10:42:51 +0200

    add theme

--- au-input/src/app/lib/au-input.module.ts
+++ au-input/src/app/lib/au-input.module.ts
@@ -2,16 +2,18 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {AuFaInputComponent} from "./au-fa-input/au-fa-input.component";
-import {InputRefDirective} from "./common/input-ref.directive";
+import { AuFaInputComponent } from "./au-fa-input/au-fa-input.component";
+import { InputRefDirective } from "./common/input-ref.directive";
 import { AuMdInputComponent } from './au-md-input/au-md-input.component';
+import { AuTabPanelComponent } from './au-tab-panel/au-tab-panel.component';
+import { AuTabComponent } from './au-tab/au-tab.component';
 
 @NgModule({
-    declarations: [AuFaInputComponent, InputRefDirective, AuMdInputComponent],
+    declarations: [AuFaInputComponent, InputRefDirective, AuMdInputComponent, AuTabPanelComponent, AuTabComponent],
     imports: [
         CommonModule
     ],
-    exports: [AuFaInputComponent, AuMdInputComponent, InputRefDirective]
+    exports: [AuFaInputComponent, AuMdInputComponent, AuTabPanelComponent, AuTabComponent, InputRefDirective]
 })
 export class AuInputModule { }
 
