91da93c33ca1672c5b16a36044f2df010ac95ccb
sylwow <Sylwo1003@gmail.com>
2022-05-09 09:38:41 +0200

    save

--- au-modal/src/app/au-modal/au-modal.module.ts
+++ au-modal/src/app/au-modal/au-modal.module.ts
@@ -2,17 +2,18 @@
 
 
 
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuModalComponent } from './au-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AuModalComponent],
   imports: [
     CommonModule
   ],
-  exports: []
+  exports: [AuModalComponent]
 })
 export class AuModalModule {
 
